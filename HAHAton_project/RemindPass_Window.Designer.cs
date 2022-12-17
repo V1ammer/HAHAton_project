43cd8e7d20c6bd97020fe3ddda2c0247af9a6ee6
Niko-n-DEV <nikoalr6@gmail.com>
2022-12-17 16:01:03 +0300

    Малая правка окна RemindPass

--- HAHAton_project/RemindPass_Window.Designer.cs
+++ HAHAton_project/RemindPass_Window.Designer.cs
@@ -32,11 +32,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.Label_Login = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
+            this.Label_Mail = new System.Windows.Forms.Label();
+            this.Button_recovery = new System.Windows.Forms.Button();
+            this.Label_Or = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // closeButton
@@ -44,7 +44,7 @@
             this.closeButton.AutoSize = true;
             this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.closeButton.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.closeButton.Location = new System.Drawing.Point(244, 9);
+            this.closeButton.Location = new System.Drawing.Point(212, 18);
             this.closeButton.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(20, 19);
@@ -58,7 +58,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(35, 31);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(173, 30);
             this.label1.TabIndex = 9;
@@ -66,26 +66,27 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(77, 90);
+            this.textBox1.Location = new System.Drawing.Point(17, 82);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(131, 20);
-            this.textBox1.TabIndex = 10;
+            this.textBox1.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(77, 127);
+            this.textBox3.Location = new System.Drawing.Point(17, 133);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(131, 20);
-            this.textBox3.TabIndex = 12;
+            this.textBox3.TabIndex = 2;
             // 
-            // label2
+            // Label_Login
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 93);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Логин";
+            this.Label_Login.AutoSize = true;
+            this.Label_Login.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label_Login.Location = new System.Drawing.Point(14, 66);
+            this.Label_Login.Name = "Label_Login";
+            this.Label_Login.Size = new System.Drawing.Size(40, 13);
+            this.Label_Login.TabIndex = 13;
+            this.Label_Login.Text = "Логин";
             // 
             // label3
             // 
@@ -95,44 +96,47 @@
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 14;
             // 
-            // label4
+            // Label_Mail
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 130);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 13);
-            this.label4.TabIndex = 15;
-            this.label4.Text = "Почта";
+            this.Label_Mail.AutoSize = true;
+            this.Label_Mail.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label_Mail.Location = new System.Drawing.Point(15, 117);
+            this.Label_Mail.Name = "Label_Mail";
+            this.Label_Mail.Size = new System.Drawing.Size(40, 13);
+            this.Label_Mail.TabIndex = 15;
+            this.Label_Mail.Text = "Почта";
             // 
-            // button1
+            // Button_recovery
             // 
-            this.button1.Location = new System.Drawing.Point(77, 164);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 23);
-            this.button1.TabIndex = 21;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Button_recovery.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button_recovery.Location = new System.Drawing.Point(54, 182);
+            this.Button_recovery.Name = "Button_recovery";
+            this.Button_recovery.Size = new System.Drawing.Size(131, 23);
+            this.Button_recovery.TabIndex = 3;
+            this.Button_recovery.Text = "Восстановить";
+            this.Button_recovery.UseVisualStyleBackColor = true;
             // 
-            // label6
+            // Label_Or
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(36, 113);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(25, 13);
-            this.label6.TabIndex = 22;
-            this.label6.Text = "или";
+            this.Label_Or.AutoSize = true;
+            this.Label_Or.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label_Or.Location = new System.Drawing.Point(159, 109);
+            this.Label_Or.Name = "Label_Or";
+            this.Label_Or.Size = new System.Drawing.Size(30, 13);
+            this.Label_Or.TabIndex = 22;
+            this.Label_Or.Text = "ИЛИ";
             // 
             // RemindPass_Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(275, 236);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(248, 236);
+            this.Controls.Add(this.Label_Or);
+            this.Controls.Add(this.Button_recovery);
+            this.Controls.Add(this.Label_Mail);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label_Login);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -152,10 +156,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Label_Login;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Label_Mail;
+        private System.Windows.Forms.Button Button_recovery;
+        private System.Windows.Forms.Label Label_Or;
     }
 }
